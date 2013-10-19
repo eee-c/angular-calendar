f661aa3d17596134dfd5212163fbf95f43f2abce
Chris Strom <github@eeecomputes.com>
2013-10-19 00:23:15 -0400

    Simple form handling

--- web/calendar.dart
+++ web/calendar.dart
@@ -7,7 +7,32 @@ class AppointmentCtrl {
   AppointmentCtrl(Scope scope) {
     scope
       ..['appointments'] = [{'time': '08:00', 'title': 'Wake Up'}]
-      ..['addAppointment'] = (){print('yo');};
+      ..['addAppointment'] = (){
+            var newAppt = fromText(scope['appointmentText']);
+            scope['appointments'].add(newAppt);
+            scope['appointmentText'] = null;
+          };
+  }
+
+  Map fromText(v) {
+    var appt = {'time': '00:00', 'title': 'New Appointment'};
+    if (v == null || v == '') return appt;
+
+    var time_re = new RegExp(r"^\s*(\d\d:\d\d)\s*$");
+    if (time_re.hasMatch(v)) {
+      appt['time'] = time_re.firstMatch(v)[1];
+      return appt;
+    }
+
+    time_re = new RegExp(r"^\s*(\d\d:\d\d)\s+(.+)$");
+    if (time_re.hasMatch(v)) {
+      appt['time'] = time_re.firstMatch(v)[1];
+      appt['title'] = time_re.firstMatch(v)[2];
+      return appt;
+    }
+
+    appt['title'] = v;
+    return appt;
   }
 }
 
