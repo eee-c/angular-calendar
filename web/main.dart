a5f662458418e8fb4633ab953478caddc85948a4
Chris Strom <github@eeecomputes.com>
2013-10-25 22:00:43 -0400

    First pass at routing
    Routing works, but am not passing route params yet

--- web/main.dart
+++ web/main.dart
@@ -1,10 +1,29 @@
 import 'package:angular/angular.dart';
+import 'package:angular/routing/module.dart';
 import 'package:angular_calendar/calendar.dart';
 
 main() {
   var calendar = new AngularModule()
     ..type(AppointmentBackend)
-    ..type(AppointmentController);
+    ..type(AppointmentController)
+    ..type(DayViewController)
+    ..type(RouteInitializer, implementedBy: CalendarRouter);
 
   ngBootstrap(module: calendar);
+}
+
+class CalendarRouter implements RouteInitializer {
+  void init(Router router, ViewFactory view) {
+    router.root
+      ..addRoute(
+          defaultRoute: true,
+          name: 'day-list',
+          enter: view('partials/day_list.html')
+        )
+      ..addRoute(
+          name: 'day-view',
+          path: '/days/:dayId',
+          enter: view('partials/day_view.html')
+        );
+  }
 }
